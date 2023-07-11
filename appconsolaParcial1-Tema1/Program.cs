f1ffd5cbc4fdd3fca5d5049d992935c07f483fb0
Usuario <Usuario@GABINETE-09>
2023-07-11 18:50:21 -0300

    Agregar archivos de proyecto.

---
+++ appconsolaParcial1-Tema1/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+double contador, area,bases,altura , volumen, radio;
+
+do
+
+{
+	Console.WriteLine("Ingresar el radio del cilindro o si se desea salir ingresar cero");
+	radio= int.Parse(Console.ReadLine());
+	Console.WriteLine("Ingresa la altura del cilindro o si se desea salir ingresar cero");
+	altura= int.Parse(Console.ReadLine());
+
+	area = 2 * Math.PI * radio * (altura + radio);
+	bases = Math.PI * radio  * radio;
+	volumen = bases * altura;
+
+}
+
+while (bases==0 && altura==0);
+
+
+
+
+
+
+
+
