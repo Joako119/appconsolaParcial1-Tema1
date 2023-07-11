7f6892ae81ef352b8df8d8b3ebcad82ae4ec0531
Joaquin Deamelio <joaquindeamelio18@gmail.com>
2023-07-11 19:49:39 -0300

    cambios 19:50

--- appconsolaParcial1-Tema1/Program.cs
+++ appconsolaParcial1-Tema1/Program.cs
@@ -15,16 +15,21 @@ do
 		radio = PedirDouble ("Ingresar el radio del cilindro o si se desea salir ingresar cero:  ");
 		
 		altura= PedirDouble ("Ingresa la altura del cilindro o si se desea salir ingresar cero:  ");
-		
-		
-		area = 2 * Math.PI * radio * (altura + radio);
-		bases = Math.PI * radio * radio;
-		volumen = bases * altura;
-		contador++;
 
-		Console.WriteLine($" El valor del area del {contador} cilindro es,  {area},  la base del mismo es {bases}  y su voumen es {volumen}");
+		if (radio > 0 && altura > 0)
+		{
+			area = 2 * Math.PI * radio * (altura + radio);
+			bases = Math.PI * radio * radio;
+			volumen = bases * altura;
+			contador++;
 
-		if (radio == 0 && altura == 0) { salir = false; }
+			Console.WriteLine($" El valor del area del {contador} cilindro es,  {area},  la base del mismo es {bases}  y su voumen es {volumen}");
+
+			
+		}
+		else if (radio == 0 && altura == 0) { salir = false; }
+		else Console.WriteLine("Los datos a ingresar deben ser mayores a 0");
+
 	}
 	catch (FormatException ){ Console.WriteLine("Error por favor verificar el fomato ingresado"); }
 	catch (OverflowException ) { Console.WriteLine("Valores ingresados muy grasden"); }
